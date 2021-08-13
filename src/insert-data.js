722a04fd3b99f6571dbb5042d0fa95576306e90b
JovinChanQy <67467498+JovinChanQy@users.noreply.github.com>
2021-08-13 17:24:28 +0800

    comment out mongoose example

--- src/insert-data.js
+++ src/insert-data.js
@@ -46,40 +46,40 @@ run().catch(console.dir);
 // const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your MongoDB deployment's connection string.
-const uri =
- "mongodb+srv://admin:123@cluster0.lyerf.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
+// const uri =
+//  "mongodb+srv://admin:123@cluster0.lyerf.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
 
-const client = new MongoClient(uri, {
-  useUnifiedTopology: true,
-});
+// const client = new MongoClient(uri, {
+//   useUnifiedTopology: true,
+// });
 
-async function run() {
-  try {
-    await client.connect();
+// async function run() {
+//   try {
+//     await client.connect();
 
-    const database = client.db('listingsAndReviews');
-    const movies = database.collection('listingsAndReviews');
+//     const database = client.db('listingsAndReviews');
+//     const movies = database.collection('listingsAndReviews');
 
-    // Query for a movie that has the title 'Back to the Future'
-    //const query = { title: 'Back to the Future' };
-    //const movie = await movies.findOne(query);
+//     // Query for a movie that has the title 'Back to the Future'
+//     //const query = { title: 'Back to the Future' };
+//     //const movie = await movies.findOne(query);
 
-    //create data schema
-const productSchema = {
-  name: String,
-  desc: Varchar,
-  price: Int32Array,
-  promo: Int16Array
+//     //create data schema
+// const productSchema = {
+//   name: String,
+//   desc: Varchar,
+//   price: Int32Array,
+//   promo: Int16Array
   
-}
+// }
 
-    console.log(product);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+//     console.log(product);
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 
