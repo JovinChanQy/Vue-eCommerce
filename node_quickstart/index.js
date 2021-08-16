e6983f9c154392b83246535b5471fb581bfe7944
JovinChanQy <67467498+JovinChanQy@users.noreply.github.com>
2021-08-16 10:53:51 +0800

    feat: Add mongodb atlas conection with mock data

---
+++ node_quickstart/index.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+"mongodb+srv://admin:123@cluster0.lyerf.mongodb.net/sample_mflix?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+
+  } catch(err) {
+
+    console.error(err);
+  }finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+   }
+}
+
+run().catch(console.dir);
